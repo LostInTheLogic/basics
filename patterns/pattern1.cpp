e69f14426501320bc8251fb9f269ac7b7653c8ae
Muhammad-Faizan <un6902090@gmail.com>
2026-07-11 15:52:13 +0500

    Format output in pattern printing program for better readability and add new empty pattern2.cpp file

--- patterns/pattern1.cpp
+++ patterns/pattern1.cpp
@@ -8,7 +8,7 @@ int main(){
     for (int i = 1; i <= number; i++)
     {
         for (int j = 1; j <= number; j++) {
-            cout << j;
+            cout << " " << j;
         }
         cout << endl;
     }
@@ -16,4 +16,3 @@ int main(){
     return 0;
 }
 
-// what do i have to do? . that pattern.. it would take a input. i will have to print lines that much time. and elements on one line should also be input number. i will have to use nested for loop for it, ikuzo!
