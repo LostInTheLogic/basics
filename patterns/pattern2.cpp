2fc7505aaaa6475593b444408a702a0064b85f2e
Muhammad-Faizan <un6902090@gmail.com>
2026-07-11 16:23:36 +0500

    Add pattern printing implementation in pattern2.cpp

--- patterns/pattern2.cpp
+++ patterns/pattern2.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main(){
     
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    for(int i = 0; i < number; i++) {
+        char ch = 'A';
+        for (int j = 0; j < number; j++){
+            cout << " " << ch;
+            ch += 1;
+        }
+        cout << endl;
+    }
     
     return 0;
 }
