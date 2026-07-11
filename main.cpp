d9d293d5fef018887aca5324a36d2455e3080227
Muhammad-Faizan <un6902090@gmail.com>
2026-07-11 15:14:15 +0500

    Add initial implementation of switch statement example in main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// Swith statments
+#include <iostream>
+using namespace std;
+int main(){
+int my_var = 2;
+
+switch (my_var)
+{
+    case 1:
+    cout << "The value of variabel was 1.";
+    break;
+    case 2:
+    cout << "The value of variabel was 2.";
+    break;
+    default:
+    cout << "The case was default";
+    break;
+}
+return 0;
+}
